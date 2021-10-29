22dcaa3dbe26179af8cc393a43245cf0f35b00cf
Aman Kushwaha <amankush.nov2000@gmail.com>
2021-10-29 23:40:22 +0530

    Delete admin.py

--- mysite/judge/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Problems, Solutions, TestCases
-
-admin.site.register(Problems)
-admin.site.register(Solutions)
-admin.site.register(TestCases)
